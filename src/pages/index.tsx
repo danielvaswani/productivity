6f77110fa4c5ef1574b7eaaec7a4820a88463497
Vaswani,Daniel D <vaswanidaniel@gmail.com>
2025-01-16 04:13:27 +0100

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,3 @@
-import { GetServerSideProps } from "next";
-
 export default function Index() {
   return <></>;
 }
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  return {
-    redirect: {
-      source: "/",
-      destination: "todos",
-      permanent: false,
-    },
-  };
-};

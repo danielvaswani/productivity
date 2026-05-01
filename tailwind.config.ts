3902c22c4c3e6e682e0a69c66da2a4eaea19ebf3
danielvaswani <vaswanidaniel@gmail.com>
2026-05-01 15:24:46 +0200

    refactor: remove old project

--- tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
-
-export default {
-  content: ["./src/**/*.tsx"],
-  plugins: [require("daisyui")],
-} satisfies Config;
